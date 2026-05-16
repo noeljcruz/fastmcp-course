5dee13bbd935b6c8b4014fb61e876adc4630966b
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2026-05-16 12:07:16 -0400

    added server code and 'hello world' tool

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
 from fastmcp import FastMCP
+
+
+mcp = FastMCP()
+
+
+@mcp.tool
+def say_hello() -> str:
+    """Says hello world"""
+    return f"Hello World!!"
+
+
+if __name__ == "__main__":
+    mcp.run()
+
