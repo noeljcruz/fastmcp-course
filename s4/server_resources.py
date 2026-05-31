9a9785a9e03fdf3d8c13d21413cf7f3ce873c2cd
Noel Cruz <noelcruz@Noels-MacBook-Pro.local>
2026-05-30 21:29:11 -0400

    created new server/client in s4 to practice handling resources

---
+++ s4/server_resources.py
@@ -0,0 +1,18 @@
+import json
+from fastmcp import FastMCP, Context
+
+
+mcp = FastMCP(name="Config Server")
+
+@mcp.resource("data://config")
+async def config() -> str:
+    return json.dumps(
+        {
+            "version": "1.0",
+            "features": ["tools", "resources"],
+        }
+    )
+
+
+if __name__ == "__main__":
+    mcp.run()
